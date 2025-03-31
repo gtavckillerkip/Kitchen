78e92a3bc027995d1ff54aed7919bd3d0beb9598
gtavckillerkip <gtavckillerkip@gmail.com>
2025-03-31 15:23:06 +0700

    Grill progress indicator

---
+++ Assets/Scripts/Gameplay/Cooking/GrillProgressVisual.cs
@@ -0,0 +1,60 @@
+﻿using Kitchen.Gameplay.Counters;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kitchen.Gameplay.Cooking
+{
+	[RequireComponent(typeof(Grill))]
+	public class GrillProgressVisual : MonoBehaviour
+	{
+		[SerializeField] private Image _back;
+		[SerializeField] private Image _frontNormal;
+		[SerializeField] private Image _frontCaution;
+
+		private Grill _grill;
+		private Image _currentFront;
+
+		private void Start()
+		{
+			_grill = GetComponent<Grill>();
+
+			_back.gameObject.SetActive(false);
+
+			_frontNormal.gameObject.SetActive(false);
+			_frontNormal.fillAmount = 0;
+
+			_frontCaution.gameObject.SetActive(false);
+			_frontCaution.fillAmount = 0;
+
+			_currentFront = null;
+
+			_grill.StateChanged += HandleStateChanged;
+			_grill.TickOfFryingTimePassed += HandleTickOfFryingPassed;
+
+		}
+
+		private void HandleStateChanged(Grill.GrillState newState)
+		{
+			_back.gameObject.SetActive(newState != Grill.GrillState.Idle);
+			_frontNormal.gameObject.SetActive(newState == Grill.GrillState.Frying);
+			_frontCaution.gameObject.SetActive(newState == Grill.GrillState.Overfrying);
+
+			_currentFront = newState switch
+			{
+				Grill.GrillState.Frying => _frontNormal,
+				Grill.GrillState.Overfrying => _frontCaution,
+				_ => null
+			};
+
+			if (_currentFront != null)
+			{
+				_currentFront.fillAmount = 0;
+			}
+		}
+
+		private void HandleTickOfFryingPassed(float fraction)
+		{
+			_currentFront.fillAmount += fraction;
+		}
+	}
+}
--- Assets/Scripts/Gameplay/Counters/Grill.cs
+++ Assets/Scripts/Gameplay/Counters/Grill.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Kitchen.Gameplay.Counters
@@ -6,24 +7,30 @@ namespace Kitchen.Gameplay.Counters
 	[RequireComponent(typeof(ConversionRecipeUser))]
 	public class Grill : Counter
 	{
-		private enum GrillState
+		public enum GrillState
 		{
 			Idle,
-			Frying
+			Frying,
+			Overfrying
 		}
 
 		private ConversionRecipeUser _conversionRecipeUser;
 
 		private Coroutine _fryingCoroutine;
-		private WaitForSeconds _fryingTimer;
 
 		private GrillState _state;
+		private float _fryingTick;
+		private float _fryingTime;
+
+		public event Action<GrillState> StateChanged;
+		public event Action<float> TickOfFryingTimePassed;
 
 		private void Start()
 		{
 			_conversionRecipeUser = GetComponent<ConversionRecipeUser>();
-			_fryingTimer = new(5);
 			_state = GrillState.Idle;
+			_fryingTick = 1;
+			_fryingTime = 3;
 		}
 
 		public override void Utilize(GameObject utilizer)
@@ -37,14 +44,17 @@ namespace Kitchen.Gameplay.Counters
 					CarriedItem = carrier.Drop();
 
 					_fryingCoroutine = StartCoroutine(Fry());
+					_state = GrillState.Frying;
+					StateChanged?.Invoke(_state);
 				}
 			}
 			else
 			{
-				if (_state == GrillState.Frying)
+				if (_state == GrillState.Frying || _state == GrillState.Overfrying)
 				{
 					StopCoroutine(_fryingCoroutine);
 					_state = GrillState.Idle;
+					StateChanged?.Invoke(_state);
 				}
 
 				if (carrier.TryTake(CarriedItem))
@@ -56,9 +66,20 @@ namespace Kitchen.Gameplay.Counters
 
 		private IEnumerator Fry()
 		{
-			_state = GrillState.Frying;
+			float passedTimeOfTick = 0;
 
-			yield return _fryingTimer;
+			for (float passedTimeOfWholeFrying = 0; passedTimeOfWholeFrying < _fryingTime; passedTimeOfWholeFrying += Time.deltaTime)
+			{
+				passedTimeOfTick += Time.deltaTime;
+
+				if (passedTimeOfTick > _fryingTick)
+				{
+					passedTimeOfTick = 0;
+					TickOfFryingTimePassed?.Invoke(_fryingTick / _fryingTime);
+				}
+
+				yield return null;
+			}
 
 			var @out = _conversionRecipeUser.UseRecipe(CarriedItem);
 
@@ -71,10 +92,13 @@ namespace Kitchen.Gameplay.Counters
 			if (_conversionRecipeUser.RecipePresent(@out))
 			{
 				_fryingCoroutine = StartCoroutine(Fry());
+				_state = GrillState.Overfrying;
+				StateChanged?.Invoke(_state);
 			}
 			else
 			{
 				_state = GrillState.Idle;
+				StateChanged?.Invoke(_state);
 			}
 		}
 	}
