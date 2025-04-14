5e6bfa433ba89de77d6d423427362102f3a335ca
gtavckillerkip <gtavckillerkip@gmail.com>
2025-04-14 14:32:53 +0700

    Ingredients sprites added

--- Assets/Scripts/ScriptableObjects definitions/Ingredients/IngredientSO.cs
+++ Assets/Scripts/ScriptableObjects definitions/Ingredients/IngredientSO.cs
@@ -6,6 +6,6 @@ namespace Kitchen.ScriptableObjects.Ingredients
 	[CreateAssetMenu(menuName = "Scriptable objects/Ingredients/Ingredient")]
 	public class IngredientSO : ItemSO
 	{
-
+		[field: SerializeField] public Sprite Sprite { get; private set; }
 	}
 }
