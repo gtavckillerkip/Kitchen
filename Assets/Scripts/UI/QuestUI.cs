885b3e5fe19d6fe9bf6b51a663ba3bf9f1a041ae
gtavckillerkip <gtavckillerkip@gmail.com>
2025-04-15 19:06:04 +0700

    Quests system finished (+ UI).

---
+++ Assets/Scripts/UI/QuestUI.cs
@@ -0,0 +1,48 @@
+﻿using Kitchen.Gameplay.Quests;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kitchen.UI
+{
+	public class QuestUI : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI _titleText;
+		[SerializeField] private TextMeshProUGUI _rewardText;
+		[SerializeField] private Image[] _ingredientsImages = new Image[4];
+
+		public  Quest RefferedQuest { get; private set; }
+
+		private void OnEnable()
+		{
+			DeactivateIngredientsImages();
+		}
+
+		public void Setup(Quest quest)
+		{
+			RefferedQuest = quest;
+
+			_titleText.text = quest.QuestSO.DishRecipeSO.Out.Name;
+			_rewardText.text = quest.QuestSO.Reward.ToString();
+
+			for (int i = 0; i < quest.QuestSO.DishRecipeSO.Ins.Length; i++)
+			{
+				_ingredientsImages[i].gameObject.SetActive(true);
+				_ingredientsImages[i].sprite = quest.QuestSO.DishRecipeSO.Ins[i].Sprite;
+			}
+		}
+
+		private void OnDisable()
+		{
+			RefferedQuest = null;
+		}
+
+		private void DeactivateIngredientsImages()
+		{
+			for (int i = 0; i < _ingredientsImages.Length; i++)
+			{
+				_ingredientsImages[i].gameObject.SetActive(false);
+			}
+		}
+	}
+}
--- Assets/Scripts/UI/QuestsUI.cs
+++ Assets/Scripts/UI/QuestsUI.cs
@@ -6,20 +6,54 @@ namespace Kitchen.UI
 {
 	public class QuestsUI : MonoBehaviour
 	{
+		[SerializeField] private QuestUI[] _questsUIs = new QuestUI[4];
+
+		private int _lastActiveQuestIndex = -1;
+
 		private void Start()
 		{
+			DeactivateAllQuestsUIs();
+
 			QuestsManager.Instance.QuestAdded += HandleQuestAdded;
 			QuestsManager.Instance.QuestRemoved += HandleQuestRemoved;
 		}
 
 		private void HandleQuestAdded(Quest quest)
 		{
+			_lastActiveQuestIndex++;
 
+			_questsUIs[_lastActiveQuestIndex].gameObject.SetActive(true);
+			_questsUIs[_lastActiveQuestIndex].Setup(quest);
 		}
 
 		private void HandleQuestRemoved(Quest quest)
 		{
+			for (int i = 0; i < _questsUIs.Length; i++)
+			{
+				if (_questsUIs[i].RefferedQuest == quest)
+				{
+					var deactivatedQuestUI = _questsUIs[i];
 
+					_questsUIs[i].gameObject.SetActive(false);
+
+					for (int j = i + 1; j < _questsUIs.Length; j++)
+					{
+						_questsUIs[j - 1] = _questsUIs[j];
+					}
+
+					_questsUIs[^1] = deactivatedQuestUI;
+					deactivatedQuestUI.transform.SetAsLastSibling();
+					_lastActiveQuestIndex--;
+				}
+			}
+		}
+
+		private void DeactivateAllQuestsUIs()
+		{
+			for (int i = 0; i < _questsUIs.Length; i++)
+			{
+				_questsUIs[i].gameObject.SetActive(false);
+			}
 		}
 	}
 }
